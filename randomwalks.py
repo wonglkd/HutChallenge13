98425ffcd99419517dc85d9f358153c7ccb1f383
Daniel Wong <wonglkd@gmail.com>
2014-02-08 18:28:05 +0000

    Introduced use of fixed seed; refactoring and PEP8 cleanup

--- randomwalks.py
+++ randomwalks.py
@@ -13,6 +13,7 @@ import bisect
 # from numpy.random import rand
 # import numpy as np
 
+
 def nx_ppr(G, customers_to_predict, products, customers):
     per_template = { node: 0 for node in G }
     for c in customers_to_predict:
@@ -24,10 +25,6 @@ def nx_ppr(G, customers_to_predict, products, customers):
         ppr = sorted([(pr[product_id], product_id) for product_id in products], reverse=True)
         print c, ','.join(p[1:] for v, p in ppr[:6])
         #pprint(ppr[:20])
-
-def weighted_choice(items, cumsums):
-    rnd = random.random() * cumsums[-1]
-    return items[bisect.bisect_right(cumsums, rnd)]
 
 # def weighted_choice(items, probas):
 #   return np_choice(items, p=probas)
@@ -62,6 +59,7 @@ def nx_load_data(edges_filename):
             customers.add(node)
     return customers, products, G
 
+
 def load_adj_list(edges_filename):
     products = set()
     customers = set()
@@ -79,62 +77,94 @@ def load_adj_list(edges_filename):
             G_w[p].append(w)
     return customers, products, G, G_w
 
-def predict(customers_to_predict, customers, G, G_w, walk_length, no_of_walks, N, output_filename):
-    cumtotals = {}
-    for v1, dt in G_w.iteritems():
-        cum_sum = 0
-        cum_sums = []
-        for w in dt:
-            cum_sum += w
-            cum_sums.append(cum_sum)
-        cumtotals[v1] = cum_sums
 
-    with open(output_filename, "wb") as f:
-        for start in customers_to_predict:
-            if start not in customers:
-                # common.print_err("Skipped, no data:", start)
-                # print start[1:] + '|{}'
-                f.write(start[1:] + '|{}\n')
-                continue
-            visited_counts = Counter()
-            for _ in xrange(no_of_walks):
-                curr = weighted_choice(G[start], cumtotals[start])
-                for _ in xrange(walk_length):
-                    visited_counts[curr] += 1
-                    curr = weighted_choice(G[curr], cumtotals[curr])
-                    curr = weighted_choice(G[curr], cumtotals[curr])
-            total_cnts = sum(visited_counts.values())
-            most_visited = ['"'+k[1:]+'":'+'{}'.format(v/float(total_cnts)) for k, v in visited_counts.most_common(N)]
-            row_str = start[1:] + '|' + '{' + ','.join(most_visited) + '}'
-            # print row_str
-            f.write(row_str + "\n")
-            # visited_counts
+class RandomWalks:
+    def __init__(self, seed):
+        self.prng = random.Random()
+        self.prng.seed(seed)
+
+    def load(self, edges_filename, graph_type='adj_list'):
+        if graph_type == 'adj_list':
+            (self.customers, self.products,
+                self.G, self.G_w) = load_adj_list(edges_filename)
+        else:
+            raise Exception("graph_type not found")
+
+        self.cumtotals = {}
+        for v1, dt in self.G_w.iteritems():
+            cum_sum = 0
+            cum_sums = []
+            for w in dt:
+                cum_sum += w
+                cum_sums.append(cum_sum)
+            self.cumtotals[v1] = cum_sums
+
+    def weighted_choice(self, items, cumsums):
+        rnd = self.prng.random() * cumsums[-1]
+        return items[bisect.bisect_right(cumsums, rnd)]
+
+    def predict(self, customers_to_predict, walk_length,
+                no_of_walks, N, output_filename):
+        with open(output_filename, "wb") as f:
+            for start in customers_to_predict:
+                if start not in self.customers:
+                    # common.print_err("Skipped, no data:", start)
+                    # print start[1:] + '|{}'
+                    f.write(start[1:] + '|{}\n')
+                    continue
+                visited_counts = Counter()
+                for _ in xrange(no_of_walks):
+                    curr = self.weighted_choice(self.G[start],
+                                                self.cumtotals[start])
+                    for _ in xrange(walk_length):
+                        visited_counts[curr] += 1
+                        for _ in xrange(2):
+                            curr = self.weighted_choice(self.G[curr],
+                                                        self.cumtotals[curr])
+                total_cnts = sum(visited_counts.values())
+                most_visited = ['"'+k[1:]+'":'+'{}'.format(v/float(total_cnts))
+                                for k, v in visited_counts.most_common(N)]
+                row_str = start[1:] + '|' + '{' + ','.join(most_visited) + '}'
+                # print row_str
+                f.write(row_str + "\n")
+                # visited_counts
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('edges_filename', default='interim/customer_product_counts.csv')
-    parser.add_argument('customers_filename', default='data/publicChallenge.csv')
+    parser.add_argument('edges_filename',
+                        default='interim/customer_product_counts.csv')
+    parser.add_argument('customers_filename',
+                        default='data/publicChallenge.csv')
     parser.add_argument('-o', '--output', default='rwalks.probas')
     parser.add_argument('-l', '--walk-length', default=2, type=int)
     parser.add_argument('-w', '--no-walks', default=20, type=int)
     parser.add_argument('-N', '--top-n', default=20, type=int)
+    parser.add_argument('-s', '--seed', default=101, type=int)
     args = parser.parse_args()
+
+    rw = RandomWalks(args.seed)
 
     # customers, products, G = nx_load_data(edges_filename)
     common.print_err("Loading...")
-    customers, products, G, G_w = load_adj_list(args.edges_filename)
+    rw.load(args.edges_filename)
     common.print_err("Loaded")
 
     # customers_to_predict = customers
 
     # with open(customers_to_predict_filename, 'rb') as f:
     #   customers_to_predict = ['c'+cid.strip() for cid in f if cid.strip() != ""]
-    customers_to_predict = customer.load_ids(args.customers_filename, add_prefix='c')
+    customers_to_predict = customer.load_ids(args.customers_filename,
+                                             add_prefix='c')
 
     # walk_length = 2
     # no_of_walks = 20 # 100
 
-    predict(customers_to_predict, customers, G, G_w, args.walk_length, args.no_walks, args.top_n, args.output)
+    rw.predict(customers_to_predict,
+               args.walk_length,
+               args.no_walks,
+               args.top_n,
+               args.output)
 
     # N = 6
     # N = 20
